48eaf6038b57b64c56070562959ded0bb64bf75d
Aleksandr.Bendrikov <bendrikoff0220@gmail.com>
2026-01-19 22:38:37 +0300

    fix

--- ecosystem.tunnel.config.js
+++ ecosystem.tunnel.config.js
@@ -3,7 +3,8 @@ module.exports = {
     {
       name: 'users-top-api-tunnel',
       script: '/usr/local/bin/cloudflared',
-      args: 'tunnel --url http://127.0.0.1:3000',
+      cwd: __dirname,
+      args: 'tunnel --config cloudflared-empty.yml --url http://127.0.0.1:3000 --no-autoupdate',
       interpreter: 'none',
       autorestart: true,
       watch: false,
